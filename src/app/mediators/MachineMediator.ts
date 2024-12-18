c46001c499dcb1ff768212182f6fab2882b70bdb
ozdenakca <akca.ozden@gmail.com>
2024-12-18 13:09:44 +0700

    added machine mediator

---
+++ src/app/mediators/MachineMediator.ts
@@ -0,0 +1,16 @@
+import { BehaviorSubject } from "rxjs";
+
+export class MachineMediator {
+  public isSpinning: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  public spinCount: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  updateIsSpinning(value: boolean): void {
+    this.isSpinning.next(value);
+  }
+
+  updateSpinCount(value: number): void {
+    this.spinCount.next(value);
+  }
+}
